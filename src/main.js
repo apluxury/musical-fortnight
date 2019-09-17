968b690379bd6f007de6b30b5800e1c201c29e28
apluxury <1091758704@qq.com>
2019-09-17 20:05:15 +0800

    swiper组件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入swiper插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 // 项目初始化
 import 'styles/reset.css'
@@ -10,11 +12,12 @@ import 'styles/reset.css'
 import 'styles/border.css'
 // 引入字体图标文件
 import 'styles/iconfont.css'
-
+import 'swiper/dist/css/swiper.css'
 // 解决移动端点击事件延迟300ms
 import fastClick from 'fastclick'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="swiperbox">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1605/ce/1a49b3c1cc81a9f7.jpg_890x330_d73469ef.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/7494fa53d36663f12d6b3ecbe4ed5ca2.jpg_890x330_a37f96fa.jpg'
+      }
+
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.swiperbox >>>.swiper-pagination-bullet-active {
+  background-color: #fff !important;
+}
+
+.swiperbox {
+  overflow: hidden;
+  width: 100%;
+  // 设置宽高比 给图片预留高度
+  padding-buttom: 31.25%;
+  background-color: #eee;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
