61f8f6a9335ac669b4b8dd91b0ed680e9d1aa69f
apluxury <1091758704@qq.com>
2019-09-19 15:08:35 +0800

    banner 组件

---
+++ src/pages/detail/componens/Banner.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="banner" @click="handlebanner">
+      <img
+        class="banner-img"
+        src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg"
+      />
+      <div class="banner-info">
+        <div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icons">&#xe616;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallery :imgs="imgs" v-show="showgallery" @close="handleclosebanner"></common-gallery>
+  </div>
+</template>
+<script>
+import CommonGallery from 'common/gallery/Gallery'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showgallery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg']
+    }
+  },
+  methods: {
+    handlebanner () {
+      this.showgallery = true
+    },
+    handleclosebanner () {
+      this.showgallery = false
+    }
+  },
+  components: {
+    CommonGallery
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.banner {
+  position: relative;
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 55%;
+
+  .banner-img {
+    width: 100%;
+  }
+
+  .banner-info {
+    display: flex;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    line-height: 0.6rem;
+    color: #fff;
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+
+    .banner-title {
+      flex: 1;
+      font-size: 0.32rem;
+      padding: 0 0.2rem;
+    }
+
+    .banner-number {
+      margin-top: 0.14rem;
+      padding: 0 0.4rem;
+      height: 0.32rem;
+      line-height: 0.32rem;
+      border-radius: 0.2rem;
+      background: rgba(0, 0, 0, 0.8);
+      font-size: 0.24rem;
+
+      .banner-icons {
+        font-size: 0.24rem;
+      }
+    }
+  }
+}
+</style>
